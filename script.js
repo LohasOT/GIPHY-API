38fd0082f36ad2b7c8de915ba4234afa80368f6f
LohasOT <oliverhoang93@gmail.com>
2021-11-18 14:50:35 -0800

    successfully worked gifs

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 
+document.getElementById('search').addEventListener('click', event => {
+  event.preventDefault()
+  axios.get(`https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC`)
+    .then(res => {
+      const gif = res.data.data
+      let random = Math.floor(Math.random() * gif.length)
 
+      console.log(gif)
+      console.log(random)
+      document.getElementById('gifspot').innerHTML = `
 
-http://api.giphy.com/v1/gifs/feqkVgjJpYtjy?api_key=dc6zaTOxFJmzC
+      <img src="${gif[random].images.original.url}">
+      `
+      console.log(gif[random].images.original.url)
+    })
+    .catch(err => console.log(err))
+})
